eb734f0e93dadd3d492ef63a4d0e1fcba20a00ac
Jakub Święch <jakub.swiech@kotrak.com>
2020-12-03 00:42:54 +0100

    Update to newer DotNetCore

--- BorrowIt.GatewayApi/BorrowIt.GatewayApi/Startup.cs
+++ BorrowIt.GatewayApi/BorrowIt.GatewayApi/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Ocelot.DependencyInjection;
@@ -27,12 +28,12 @@ namespace BorrowIt.GatewayApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllers();
             services.AddOcelot(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -45,7 +46,7 @@ namespace BorrowIt.GatewayApi
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+
             await app.UseOcelot();
         }
     }
